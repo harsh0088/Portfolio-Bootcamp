241b038bb1eea8998475e8de20a5d5f7bf629554
harsh0088 <105282771+harsh0088@users.noreply.github.com>
2022-05-16 01:39:02 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,8 @@
+function greeting(heading) {
+    return (showHead.innerHTML=heading);
+      }
+      function userData(callback) {
+          heading='My creative skills and experience';
+          callback(heading);
+      }
+      userData(greeting);
